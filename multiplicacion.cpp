28d5250908be10c9a172d60d350bad1d5e89f077
M@rc0_Sama <130240200+Mrc0sc11@users.noreply.github.com>
2025-03-16 12:29:48 -0600

    Clase Multiplicación Marco

---
+++ multiplicacion.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+using namespace std;
+
+class multiplicacion
+{
+private:
+    /* data */
+public:
+    multiplicacion(/* args */);
+    ~multiplicacion();
+};
+
+multiplicacion::multiplicacion(/* args */)
+{
+}
+
+multiplicacion::~multiplicacion()
+{
+}
